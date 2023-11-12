4f2eff99897634529d2350cfa9e6d065334ed727
DayLayKuny <haykkazaryan2007@gmail.com>
2023-11-12 13:58:17 +0400

    update

---
+++ src/components/header/AppHeader.jsx
@@ -0,0 +1,21 @@
+import "./AppHeader.css"
+
+function AppHeader () {
+    return (
+        <div className="all-header">
+            <div className="downstream-header">
+                <div className="logo-div">
+                    <img src="" alt=""></img>
+                </div>
+                <div className="info-div">
+
+                </div>
+                <div className="login-div">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AppHeader

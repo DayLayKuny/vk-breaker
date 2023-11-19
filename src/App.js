b878f5856c04cababc4adc1ece16e08057303f45
dawitmanukian <davit.manukyan.d@tumo.org>
2023-11-19 16:15:43 +0400

    updates

--- src/App.js
+++ src/App.js
@@ -10,19 +10,25 @@ import Bitcoin from "./components/Bitcoin/Bitcoin";
 import Footer from "./components/footer/Footer";
 import Alert from "./components/alert/Alert";
 import Loading from "./components/loading/Loading";
+import ErrorAlert from "./components/error-alert/ErrorAlert";
 
 function App() {
   const [openAuth, setOpenAuth] = useState(false);
   const [openAlert, setOpenAlert] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   return (
     <>
       {openAuth && <AuthForm onClose={() => setOpenAuth(false)} />}
       {openAlert && <Alert />}
       {loading && <Loading />}
+      {error && <ErrorAlert />}
       <AppHeader onOpen={() => setOpenAuth(true)} />
-      <HeaderSection onHacking={() => setLoading(true)}/>
+      <HeaderSection onHacking={() => {
+        setLoading(true)
+        setError(false)
+      }} onError={() => setError(true)}/>
       <Compat />
       <Amount />
       <Bitcoin />
--- src/components/error-alert/ErrorAlert.jsx
+++ src/components/error-alert/ErrorAlert.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
+import '../../components/alert/alert.css'
+import { TbAlertTriangleFilled } from "react-icons/tb";
 
 const ErrorAlert = () => {
   return (
-    <div>
-        <div>
-            
+    <>
+      <div style={{ width: "400px" }}>
+        <div className="alert error">
+          <TbAlertTriangleFilled style={{ color: "red", fontSize: "25px" }}/>
+          <span>Некорректная ссылка !</span>
         </div>
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default ErrorAlert
+export default ErrorAlert;

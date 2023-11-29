8ab9bc7b17818d94455680a99fae45dff5c66eb5
dawitmanukian <davit.manukyan.d@tumo.org>
2023-11-29 21:36:45 +0400

    updates

--- src/components/header/AppHeader.jsx
+++ src/components/header/AppHeader.jsx
@@ -2,27 +2,32 @@ import LeftMenu from "../left-menu/LeftMenu";
 import "./AppHeader.css";
 import logo from "../../img/logo+name.svg";
 import { Link } from "react-router-dom";
+import AuthForm from "../auth-form/AuthForm";
 
-function AppHeader({ onOpen, open }) {
+function AppHeader({ onOpen, open, onClose }) {
   return (
-    <div className="all-header">
-      <div className="downstream-header container">
-        <Link to={"/"}>
-          <img src={logo} height={"100px"} width={"200px"}/>
-        </Link>
-        <div className="info-div">
-          <Link to="/price">Стоимость</Link>
-          <Link to="/faq">FAQ</Link>
-          <Link to="/support">Поддержка</Link>
-          <Link to="#">Блог</Link>
-          <Link to="/user">User</Link>
+    <>
+    {console.log(open)}
+      {open && <AuthForm onClose={() => onClose()}/>}
+      <div className="all-header">
+        <div className="downstream-header container">
+          <Link to={"/"}>
+            <img src={logo} height={"100px"} width={"200px"} />
+          </Link>
+          <div className="info-div">
+            <Link to="/price">Стоимость</Link>
+            <Link to="/faq">FAQ</Link>
+            <Link to="/support">Поддержка</Link>
+            <Link to="#">Блог</Link>
+            <Link to="/user">User</Link>
+          </div>
+          <div className="login-div">
+            <button onClick={() => onOpen()}>Вход / Регистрация</button>
+          </div>
+          <LeftMenu id="left-menu" onOpen={() => onOpen()} />
         </div>
-        <div className="login-div">
-          <button onClick={() => onOpen()}>Вход / Регистрация</button>
-        </div>
-        <LeftMenu id="left-menu" onOpen={() => onOpen()} />
       </div>
-    </div>
+    </>
   );
 }
 
